03339956b08cc70d789c3d93769165c62550b009
FosterClark48 <fosterclark48@yahoo.com>
2022-10-27 10:51:21 -0700

    Initial header file commit

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+int _putchar(char c);
+
+#endif
