cd23b477039b6810ef53a6a53fd011b0be54f926
FosterClark48 <fosterclark48@yahoo.com>
2022-11-01 12:25:46 -0700

    added unistd lib

--- main.h
+++ main.h
@@ -1,14 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * #include <unistd.h>
- * #include <stdlib.h>
-*/
-
+#include <unistd.h>
 #include <stdarg.h>
 
 /**
+ * #include <stdlib.h>
  * #include <limits.h>
  */
 
